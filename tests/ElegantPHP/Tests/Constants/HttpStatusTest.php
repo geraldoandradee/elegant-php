3d1d5dd4e60742699436accc89e0adfc520ba31b
Geraldo Andrade <geraldo@geraldoandrade.com>
2016-05-29 15:28:41 -0300

    More tests on HttpStatus class

--- tests/ElegantPHP/Tests/Constants/HttpStatusTest.php
+++ tests/ElegantPHP/Tests/Constants/HttpStatusTest.php
@@ -9,9 +9,12 @@ class HttpStatusTest extends \PHPUnit_Framework_TestCase
 {
     public function testMessage() {
         $h = new HttpStatus(200);
+        $this->assertEquals(200, $h->getCode(), 'It must be 200');
+        $this->assertEquals('OK', HttpStatus::getMessageFromCode(200), 'It must be OK');
         $this->assertEquals('OK', $h->getMessage(), 'Message must be "OK"');
 
         $h->setCode(301);
+        $this->assertEquals(301, $h->getCode(), 'It must be 301');
         $this->assertEquals('OK', $h->getMessage(), 'Message must be "OK"');
         $this->assertEquals('Moved Permanently', $h->getMessageCode(), 'Message must be "Moved Permanently"');
     }
